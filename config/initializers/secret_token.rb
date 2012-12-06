fd732f7d2048cdad5dd862d03f0be47c06bc594e
Cloudfactory Workstation <cloudfactory@Cloudfactory-Workstations-iMac-3.local>
2012-12-06 10:54:35 +0545

    Create a new rails project repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsproj::Application.config.secret_token = '5739926fd20ac9f7ac4672f552a897dbe9cd4f0170439793129a7be09c7d5fe1481028e56e281fdc7b6cbd6d8c9f8908ba7a8393a85bcd87763345f0a23fefe2'
